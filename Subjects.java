dbc98a9a10cb61f145b67ccebe4c3a7b3f50d700
Janjan Tjan <Janjan Tjan>
2017-01-05 15:36:57 -0500

    V2

--- Subjects.java
+++ Subjects.java
@@ -2,36 +2,32 @@ import java.io.File;
 import java.io.FileNotException;
 import java.io.util.Scanner;
 
-
-
 public class Subjects {
 
     private String tests;
-    pri
     private String quizzes;
     private String projects;
     private String homework;
-    private int[] breakdown;
+    private String  breakdown;
     private String name;
 
-    public Subject(String n, String te, String qu, String pr, String hw, int t, int q, int p, int h){
+    public Subject(String n, String b, String te, String qu, String pr, String hw){
 	name = n;
 	String tests = te;
 	String quizzes = qu;
 	String projects = pr;
 	String homework = hw;
-	int[] breakdown = new int[4];
-	breakdown[0]= t;
-	breakdown[1]= q;
-	breakdown[2]= p;
-	breakdown[3]= h;
+	String breakdown = b;
     }
 
-    public getName() {
+    public String  getName() {
 	return name;
     }
 
-    public getTests()
+    public int[] getTests() {
+	
+    }
+    
 
     
 
