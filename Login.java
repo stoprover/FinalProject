2a7a04ef78aec89c827fb1951a0489b4e8f8a406
stoprover <steventoprover@gmail.com>
2017-01-06 15:32:40 -0500

    cardlayout

---
+++ Login.java
@@ -0,0 +1,8 @@
+import java.util.Scanner
+public class Login{
+    Scanner s = new Scanner(passwords.csv).useDelimiter("\\s*,\\s*");
+    String userpass = s.next();
+    /** 
+	http://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
+    */
+}
---
+++ LoginGUI.java
@@ -0,0 +1,28 @@
+package Layout;
+import javax.swing.*;
+import java.awt.*;
+public class LoginGUI implements Itemlistener{
+    JPanel cards;
+    final static String Login = "Log in!";
+    final static String CreateAcc = "Create an account!";
+    public void addComp (Container pane){
+	JPanel comboBoxPane = new JPanel();
+String 
+    private Container pane;
+    private JCheckBox pickAnAct;
+    private JButton lesgo;
+    private JLabel username;
+    private JLabel passname;
+    private JTextField userinput;
+    private JTextField passinput;
+    public LoginGUI(){
+	this.setTitle("Log in or sign up");
+	this.setSize(600,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	pane.setLayout(new CardLayout());
+
+pickAnAct = new JCheck
+	lesgo = new JButton("Let's go!");
