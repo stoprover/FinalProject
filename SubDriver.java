89dbfb9dac74326c988c69a7803cb591da3a3777
janjantjan <janjan.tjan@gmail.com>
2017-01-08 21:42:36 -0500

    Stuff

--- SubDriver.java
+++ SubDriver.java
@@ -1,3 +1,10 @@
 public class SubDriver {
+    public static void main (String[]args) {
+	Subjects Physics =  new Subjects("Physics", "50.20.20.10.", "98.87.81.92.95.", "100.100.80.80.", "100.", "90.100.90.");
 
-    Subjects Physics =  new Subjects(Physics, 91.80.85., 90.90.90., 100., 
+	System.out.println (Physics.getName());
+	System.out.println (toString(Physics.getTests()));
+	System.out.println (toString(Physics.getQuizzes()));
+    }
+}
+	
--- Subjects.java
+++ Subjects.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileNotException;
-import java.io.util.Scanner;
+
 
 public class Subjects {
 
@@ -14,52 +12,179 @@ public class Subjects {
 
   
 
-	public Subjects(String n, String te, String qu, String pr, String hw, int t, int q, int p, int h){
+    public Subjects(String n, String b, String te, String qu, String pr, String hw){
 
 	    name = n;
-	    String tests = te;
-	    String quizzes = qu;
-	    String projects = pr;
-	    String homework = hw;
-	    String breakdown = b;
+	    tests = te;
+	    quizzes = qu;
+	    projects = pr;
+	    homework = hw;
+	     breakdown = b;
 	}
     
 
 	public String  getName() {
 	    return name;
 	}
-
-
-	public int[] getTests() {
-	
-	}
-
     
-	public getTests(){
-	    int dotAmount = 0;
-	    for (int i = 0; i < tests.length(); i++){
-		if (tests.charAt(i) == '.') {
-		    dotAmount++;}
-	    }
+    public int[] getBreakdown(){
+	int dotAmount = 0;
 
-	    int[] tG = new int[dotAmount];
-
-	
-	    for (int i = 0; i < tG.length; i++){
-		int index = 0;
-		String temp = tests;
-		String val = "";	    
-		int end = 0;
-		for (; tests.charAt(i) != '.'; end++) {
-		    end++;
-		}
-		val = tests.substring(0,end);
-		int intval = Integer.parseInt(val);
-		tG[index] = intval;
-		index++;
-	    }
+	for (int i = 0; i <  breakdown.length(); i++){
+	    if (breakdown.charAt(i) == '.') {
+		dotAmount++;}
 	}
+
+	int[] b = new int[dotAmount];
+
+	String temp = breakdown;
+
+	for (int index = 0; temp.length()> 0; index++){
+	    String val = "";
+	    int end =0;
+	    for (int i = 0; temp.charAt(i) != '.'; i++) {
+		end++;
+	    }
+	    val = temp.substring(0,end);
+	    int intval = Integer.parseInt(val);
+	    b[index] = intval;
+	    temp = temp.substring(end+1);
+	}  
+
+	return b;
     }
+    
+    public int[] getTests(){
+	int dotAmount = 0;
+
+	for (int i = 0; i <  tests.length(); i++){
+	    if (tests.charAt(i) == '.') {
+		dotAmount++;}
+	}
+
+	int[] tG = new int[dotAmount];
+
+	String temp = tests;
+
+	for (int index = 0; temp.length()> 0; index++){
+	    String val = "";
+	    int end =0;
+	    for (int i = 0; temp.charAt(i) != '.'; i++) {
+		end++;
+	    }
+	    val = temp.substring(0,end);
+	    int intval = Integer.parseInt(val);
+	    tG[index] = intval;
+	    temp = temp.substring(end+1);
+	}  
+
+	return tG;
+    }
+
+     public int[] getQuizzes(){
+	int dotAmount = 0;
+
+	for (int i = 0; i <  quizzes.length(); i++){
+	    if (quizzes.charAt(i) == '.') {
+		dotAmount++;}
+	}
+
+	int[] qG = new int[dotAmount];
+
+	String temp = quizzes;
+
+	for (int index = 0; temp.length()> 0; index++){
+	    String val = "";
+	    int end =0;
+	    for (int i = 0; temp.charAt(i) != '.'; i++) {
+		end++;
+	    }
+	    val = temp.substring(0,end);
+	    int intval = Integer.parseInt(val);
+	    qG[index] = intval;
+	    temp = temp.substring(end+1);
+	}  
+
+	return qG;
+    }
+
+     public int[] getProjects(){
+	int dotAmount = 0;
+
+	for (int i = 0; i <  projects.length(); i++){
+	    if (projects.charAt(i) == '.') {
+		dotAmount++;}
+	}
+
+	int[] pG = new int[dotAmount];
+
+	String temp = projects;
+
+	for (int index = 0; temp.length()> 0; index++){
+	    String val = "";
+	    int end =0;
+	    for (int i = 0; temp.charAt(i) != '.'; i++) {
+		end++;
+	    }
+	    val = temp.substring(0,end);
+	    int intval = Integer.parseInt(val);
+	    pG[index] = intval;
+	    temp = temp.substring(end+1);
+	}  
+
+	return pG;
+    }
+
+     public int[] getHomework(){
+	int dotAmount = 0;
+
+	for (int i = 0; i <  homework.length(); i++){
+	    if (homework.charAt(i) == '.') {
+		dotAmount++;}
+	}
+
+	int[] hG = new int[dotAmount];
+
+	String temp = homework;
+
+	for (int index = 0; temp.length()> 0; index++){
+	    String val = "";
+	    int end =0;
+	    for (int i = 0; temp.charAt(i) != '.'; i++) {
+		end++;
+	    }
+	    val = temp.substring(0,end);
+	    int intval = Integer.parseInt(val);
+	    hG[index] = intval;
+	    temp = temp.substring(end+1);
+	}  
+
+	return hG;
+    }
+
+     private static String toString(int[] ary){
+      String x = "[ ";
+      for(int i = 0; i < ary.length; i++){
+	  if (i < ary.length-1){
+	  x = x + ary[i] + ", ";
+	  }
+	  else { x = x + ary[ary.length-1] + "]";}
+      }
+      return x;
+     }
+
+    public static void main (String[]args) {
+	Subjects Physics =  new Subjects("Physics", "50.20.20.10.", "98.87.81.92.95.", "100.100.80.80.", "100.", "90.100.90.");
+
+	System.out.println (Physics.getName());
+	System.out.println (toString(Physics.getBreakdown()));
+	System.out.println (toString(Physics.getTests()));
+	System.out.println (toString(Physics.getQuizzes()));
+	System.out.println (toString(Physics.getProjects()));
+	System.out.println (toString(Physics.getHomework()));
+    }
+
+}
 	
 	
 
