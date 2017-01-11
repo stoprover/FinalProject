ae21a10053abb0914e36b8d8e313d84b551a0479
Galina Gorina <Galina Gorina>
2017-01-10 23:08:16 -0500

    working GUI and password-check

--- Login.java
+++ Login.java
@@ -6,48 +6,58 @@ import java.util.ArrayList;
 public class Login{
     String username;
     String password;
-    Boolean goodPass;
-    Boolean goodUser;
-    Boolean goOn;
+    //Boolean goodPass;
+    //Boolean goodUser;
+    //Boolean goOn;
     //maybe private, maybe not
     public Login(String u, String p){
 	username = u;
 	password = p;
     }
-    public void readFile(String name){
+    public String readFile(String name){
 	try{
 	    Scanner s = new Scanner(new File (name));
-	    goOn = true;//int i = 0;
-	    while (goOn && s.hasNextLine()){//inefficient loop bc loops thru users even after the right one is found
+	    //goOn = true;//int i = 0;
+	    while (s.hasNextLine()){//inefficient loop bc loops thru users even after the right one is found
 		/**i++;
 	    }
 	    for (int j = 0; j < i; j++){*/
 		String nextAcc = s.nextLine();
 		String nextUser = nextAcc.substring(0, nextAcc.indexOf(','));
-		System.out.println("|" + nextUser + "|");
-		System.out.println("|" + username + "|");
+		if (username.equals("")){
+		    return "Empty User";
+		}
+		if (password.equals("")){
+		    return "Empty Pass";
+		}
+		//System.out.println("|" + nextUser + "|");
+		//System.out.println("|" + username + "|");
 		if (nextUser.equals(username)){
 		    String restOfAcc = nextAcc.substring(nextAcc.indexOf(',') + 1, nextAcc.length());
 		    String nextPass = restOfAcc.substring(0, restOfAcc.indexOf(','));
-		    System.out.println("|" + nextPass + "|");
-		    System.out.println("|" + password + "|");
+		    //System.out.println("|" + nextPass + "|");
+		    //System.out.println("|" + password + "|");
 		    if (nextPass.equals(password)){
-			goodPass = true;
-			System.out.println("Success");
-			goOn = false;
+			//goodPass = true;
+			//System.out.println("Success");
+			return "Success";
+			//goOn = false;
 			//j = i;
 		    }
 		    else {
-			goodPass = false;
+			/**goodPass = false;
 			System.out.println("Bad Pass");
 			//j = i;
-			goOn = false;
+			goOn = false;*/
+			return "Bad Pass";
 		    }
 		}
 		if (!(s.hasNextLine())){
-		    goodUser = false;
-		    System.out.println("Bad User");
+		    /** goodUser = false;
+			System.out.println("Bad User");*/
+		    return "Bad User";
 		}
+		//return "Error";
 		    
 		//System.out.println(nextAcc);
 		//System.out.println(s.next());
@@ -56,14 +66,15 @@ public class Login{
 	    System.out.println("No such file");
 	    System.exit(1);
 	}
+	return "error";
     }
     public static void main(String[]args){
 	Login l = new Login("Bob", "1234567890");
-	l.readFile("Example.csv");
+	System.out.println(l.readFile("Example.csv"));
 	Login m = new Login("Bob", "lkjbsdf");
-	m.readFile("Example.csv");
+	System.out.println(m.readFile("Example.csv"));
 	Login n = new Login("sdlfk", "lkg");
-	n.readFile("Example.csv");
+	System.out.println(n.readFile("Example.csv"));
     }
 }
 /**import java.util.Scanner;
--- LoginGUI.java
+++ LoginGUI.java
@@ -8,10 +8,10 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class LoginGUI extends JFrame{
-    //some standardization: sign up/sign in refers to creating an account, log in refers to already having one
+    //some standardization: sign up & sign in refer to creating an account, while log in refers to already having one and accessing it
     //to-do's: if there is already such a username as being inputted to signup, forbid it
-    //password field (later problem)
     //possibility: make login buttons switch to a third card: janices.
+    //change passLogInput and passSignInput to JPasswordField, rather than JTextField, and fix related methods in getting their info.
     private JFrame frame = new JFrame("LogSign");
     private JPanel panelBoth = new JPanel();
     private JPanel panelLog = new JPanel();
@@ -19,19 +19,23 @@ public class LoginGUI extends JFrame{
     private JLabel userLogLabel = new JLabel ("Username: ");
     private JLabel passLogLabel = new JLabel ("Password: ");
     private JTextField userLogInput = new JTextField(12);
-    private JPasswordField passLogInput = new JPasswordField(12);
-    private JTextField logMessage = new JTextField(12);
+    private JTextField passLogInput = new JTextField(12);
+    //private JPasswordField passLogInput = new JPasswordField(12);
+    private JTextField logMessage = new JTextField(30);
     private JButton toLogIn = new JButton ("Log in!");
     private JButton switchToSign = new JButton ("Sign up instead");
     private JLabel userSignLabel = new JLabel ("Username: ");
     private JLabel passSignLabel = new JLabel ("Password: ");
     private JTextField userSignInput = new JTextField(12);
-    private JPasswordField passSignInput = new JPasswordField(12);
-    private JTextField signMessage = new JTextField(12);
+    private JTextField passSignInput = new JTextField(12);
+    // private JPasswordField passSignInput = new JPasswordField(12);
+    private JTextField signMessage = new JTextField(30);
     private JButton toSignUp = new JButton("Sign up!");
     private JButton switchToLog = new JButton ("Log in instead");
-    Boolean goodPass;
-    Boolean goodUser;
+    //Boolean goodPass;
+    //Boolean goodUser;
+    //String username;
+    //String password;
     CardLayout cl = new CardLayout();
 
     public LoginGUI(){
@@ -61,10 +65,44 @@ public class LoginGUI extends JFrame{
 	toLogIn.addActionListener (new ActionListener() {
 		@Override
 		public void actionPerformed(ActionEvent arg0){
-		    String apple = userLogInput.getText();
+		    String username = userLogInput.getText();
+		    String password = passLogInput.getText();
+		    /**int i = 0;
+		    String password = "";
+		    while(i < passLogInput.length){
+			password += passLogInput[i];
+			}*/
+		    /**if (username.length() == 0){
+			logMessage.setText("Input username");
+		    }
+		    if (password.length() == 0){
+			logMessage.setText("Input password");
+			}*/
+		    Login backend = new Login(username, password);
+		    String results = backend.readFile("Example.csv");
+		    if (results.equals("Success")){
+			dispose();
+			new LoginGUI();//change this to Janices gui eventually
+		    }
+		    else if (results.equals("Bad Pass")){
+			logMessage.setText("The password inputted does not match this username.");
+		    }
+		    else if (results.equals("Bad User")){
+			logMessage.setText("There is no such user.");
+		    }
+		    else if (results.equals("Empty User")){
+			logMessage.setText("Input your username.");
+		    }
+		    else if (results.equals("Empty Pass")){
+			logMessage.setText("Input your password.");
+		    }
+		    else{
+			logMessage.setText("Error. Please input a username and password.");
+		    }
+		    /**String apple = userLogInput.getText();
 		    userSignInput.setText(apple);
 		    dispose();//this should close the window, but that's a minor technicality
-		    new LoginGUI();//this shoudl actually be nwhatever Janice chooses to be her main GUI.
+		    new LoginGUI();//this shoudl actually be nwhatever Janice chooses to be her main GUI.*/
 
 		}
 	    });
@@ -95,6 +133,7 @@ public class LoginGUI extends JFrame{
 	System.out.println(text);
 	return text;
     }
+    
 
     public static void main(String[]args){
         LoginGUI l = new LoginGUI();
