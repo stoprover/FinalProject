d88f6a3c2e574c3375ee8383ac7cbe0bec2f3ceb
Galina Gorina <Galina Gorina>
2017-01-15 00:45:41 -0500

    trying to fix stuff

--- LoginGUI.java
+++ LoginGUI.java
@@ -82,8 +82,8 @@ public class LoginGUI extends JFrame{
 		    String results = backend.readFile("Example.csv");
 		    if (results.equals("Success")){
 			dispose();
-			new LoginGUI();
-			//new GUI("Example.csv", username);
+			//new LoginGUI();
+			new GUI("Example.csv", username);
 		    }
 		    else if (results.equals("Bad Pass")){
 			logMessage.setText("Messages: The password inputted does not match this username.");
@@ -123,8 +123,8 @@ public class LoginGUI extends JFrame{
 		    String results = create.writeFile("Example.csv");
 		    if (results.equals("Success")){
 			dispose();
-			new LoginGUI();
-			//new GUI("Example.csv", username);
+			//new LoginGUI();
+			new GUI("Example.csv", username);
 		    }
 		    else if (results.equals("Empty User")){
 			signMessage.setText("Messages: Input a username.");
